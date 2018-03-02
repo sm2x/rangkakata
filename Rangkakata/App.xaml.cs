0b8d4dd470c775b0271d894534642954036d5d69
Mazi Muhlari <mazimuhlari@Mazis-MacBook-Pro.local>
2018-03-02 19:26:56 +0200

    Updates for some Security

--- Rangkakata/App.xaml.cs
+++ Rangkakata/App.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using Xamarin.Forms;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 namespace Rangkakata
 {
     public partial class App : Application
@@ -13,7 +17,7 @@ namespace Rangkakata
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+            AppCenter.Start("android=410d0382-00e8-40d6-b563-18af6124c29e;" + "ios=36bb0eb3-8846-4eeb-84a4-a2be8900ce37", typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
