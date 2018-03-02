ba89910f097ee083c7d81da1984d82cab7422039
Mazi Muhlari <mazimuhlari@Mazis-MacBook-Pro.local>
2018-03-02 18:39:05 +0200

    Initial Commit

---
+++ Rangkakata/RangkakataPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Rangkakata
+{
+    public partial class RangkakataPage : ContentPage
+    {
+        public RangkakataPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
