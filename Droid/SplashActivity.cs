27ae08090174d636ab388df6162d9811f7cf9db4
Mazi Muhlari <mazimuhlari@Mazis-MacBook-Pro.local>
2018-03-03 16:27:05 +0200

    gradient and splash imagews

--- Droid/SplashActivity.cs
+++ Droid/SplashActivity.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+
 namespace Rangkakata.Droid
 {
-    public class SplashActivity
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        public SplashActivity()
+
+        public override void OnCreate(Android.OS.Bundle savedInstanceState, Android.OS.PersistableBundle persistentState)
         {
+            base.OnCreate(savedInstanceState, persistentState);
         }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task start = new Task(() => { Start(); });
+            start.Start();
+        }
+
+        async void Start()
+        {
+            await Task.Delay(3000);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
     }
 }
