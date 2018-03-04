7aa93cf8181343903724d3f5e0a7e69b7035505c
Mazi Muhlari <mazimuhlari@Mazis-MacBook-Pro.local>
2018-03-05 00:22:45 +0200

    Profile Complete

--- Rangkakata/Pages/ProfilePage.xaml.cs
+++ Rangkakata/Pages/ProfilePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Rangkakata.ViewModels;
+using Rangkakata.ViewModels.Core;
 using Xamarin.Forms;
 
 namespace Rangkakata.Pages
@@ -9,7 +10,19 @@ namespace Rangkakata.Pages
     {
         public ProfilePage()
         {
+            ViewModel = new ProfilePageViewModel(new PageService());
             InitializeComponent();
+        }
+
+        void BackOnTapGestureRecognizerTapped(object sender, System.EventArgs e)
+        {
+            ViewModel.GoBackToHomeCommand.Execute(null);
+        }
+
+        public ProfilePageViewModel ViewModel
+        {
+            get { return BindingContext as ProfilePageViewModel; }
+            set { BindingContext = value; }
         }
     }
 }
--- Rangkakata/ViewModels/ProfilePageViewModel.cs
+++ Rangkakata/ViewModels/ProfilePageViewModel.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Rangkakata.ViewModels.Core;
+using Xamarin.Forms;
+
 namespace Rangkakata.ViewModels
 {
-    public class ProfilePageViewModel
+    public class ProfilePageViewModel : BaseViewModel
     {
-        public ProfilePageViewModel()
+
+        private readonly IPageService _pageService;
+
+        public ICommand GoBackToHomeCommand { get; private set; }
+
+        public ProfilePageViewModel(IPageService pageService)
         {
+            _pageService = pageService;
+
+            GoBackToHomeCommand = new Command(async obj => await GoBackToHome(obj));
         }
+
+        private async Task GoBackToHome(Object obj)
+        {
+            await _pageService.PopAsync();
+        }
+
     }
 }
